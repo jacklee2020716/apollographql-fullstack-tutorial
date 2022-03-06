bdb4c600167382c87cf448f1c04dbe71083b4da4
jacklee2020716 <jacklee2020716@gmail.com>
2022-03-07 03:00:17 +0900

    Initial commit

---
+++ start/server/src/schema.js
@@ -0,0 +1,64 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: Author
+  }
+
+  type Author {
+    name: String
+    books: [Book]
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+    token: String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): User
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+`;
+
+module.exports = typeDefs;
